7e62dd477488656036eff095f082c892b4c187af
Fady <Fadyromany012@gmail.com>
2019-12-13 16:51:54 -0800

    visitaweb

---
+++ js/sessin.js
@@ -0,0 +1,18 @@
+$("#p1").slideToggle();
+$("#p2").slideToggle();
+$("#p3").slideToggle();
+
+$(document).ready(function(){
+    $("#btn1").click(function(){
+      $("#p1").slideToggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#btn2").click(function(){
+      $("#p2").slideToggle();
+    });
+  });$(document).ready(function(){
+    $("#btn3").click(function(){
+      $("#p3").slideToggle();
+    });
+  });
